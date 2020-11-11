8569e88007fbbaf2eabf1d4a05411015ed126777
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-11-11 21:13:52 +0530

    final1

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,9 +2,8 @@ const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
     // on Windows you might want to set publicPath: "http://127.0.0.1:8080/" 
-    transpileDependencies: ["vuetify"],
-    publicPath: "http://127.0.0.1:8080/", 
-    outputDir: '/dist/',
+    publicPath: "http://0.0.0.0:8080/", 
+    outputDir: './dist/',
 
     chainWebpack: config => {
 
@@ -34,12 +33,12 @@ module.exports = {
 
     },
 
-    //uncomment before executing 'npm run build' 
-    css: {
-        extract: {
-          filename: 'bundle.css',
-          chunkFilename: 'bundle.css',
-        },
-    }
+    // uncomment before executing 'npm run build' 
+    // css: {
+    //     extract: {
+    //       filename: 'bundle.css',
+    //       chunkFilename: 'bundle.css',
+    //     },
+    // }
 
 };
