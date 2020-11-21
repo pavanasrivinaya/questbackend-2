3065512f657e1c5a855a795f4e6a97e8782de119
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-11-21 10:27:21 +0530

    last

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
     // on Windows you might want to set publicPath: "http://127.0.0.1:8080/" 
-    publicPath: "http://127.0.0.1:8080/", 
+    publicPath: "https://questbackend-2.herokuapp.com/", 
     outputDir: './dist/',
 
     chainWebpack: config => {
@@ -22,8 +22,8 @@ module.exports = {
 
         config.devServer
             // the first 3 lines of the following code have been added to the configuration
-            .public('http://127.0.0.1:8080')    
-            .host('127.0.0.1')    
+            .public('https://questbackend-2.herokuapp.com')    
+            .host('questbackend-2.herokuapp.com')    
             .port(8080)
             .hotOnly(true)
             .watchOptions({poll: 1000})
